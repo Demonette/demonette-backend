1c308b34657dd4ed8b1486ec1f53f76d18e87969
Simon Meoni <simonmeoni@aol.com>
2018-03-19 16:27:09 +0100

    write all functionality to return the auto-suggestion of the server. it lacks some unmapped fields. Waiting ....

--- methods/autocomplete.js
+++ methods/autocomplete.js
@@ -5,40 +5,78 @@ const elasticEnv = process.env.ELASTIC_DEMONETTE;
 
 const client = new elasticsearch.Client({
   host: `${elasticEnv}`,
-  log: 'trace',
 });
 
-module.exports = function searchOnElasticDb(token, field) {
+function formatAggs(resp) {
+  const res = [];
+  resp.aggregations.dedup.buckets.forEach(el =>
+    res.push(Object.values(el.dedup_docs.hits.hits[0]._source)[0]));
+  return res;
+}
+
+function searchTokenInField(field, token, acObject) {
+  const newObject = acObject;
   const aggs = {
     'dedup': {
       'terms': {
-        'field': field,
+        'size': 8,
+        'field': `${field}`,
       },
       'aggs': {
         'dedup_docs': {
           'top_hits': {
             'size': 1,
-            '_source': field,
+            '_source': [`${field}`],
           },
         },
       },
     },
   };
+
   const query = {
     'query_string': {
+      'fields': [
+        `${field}`,
+      ],
       'query': `*${token}*`,
-      'fields': [field],
     },
   };
+
   return client.search({
     index: 'demonette',
     type: 'relation',
-    _source: field,
     body: {
-      'size': 0,
       'query': query,
+      'size': 0,
       'aggs': aggs,
     },
-  }).then(resp => resp.aggregations.dedup.buckets.map(aggregation =>
-    (aggregation.dedup_docs.hits.hits[0]._source[field])));
+  }).then((resp) => {
+    newObject[field] = formatAggs(resp);
+    return acObject;
+  });
+}
+
+function format(res) {
+  Object.keys(res).forEach((k) => {
+    const resKey = k.split('_')[0];
+    if (k.indexOf('_') !== -1) {
+      if (!(k.split('_')[0] in res)) {
+        res[resKey] = [];
+      }
+      res[resKey] = Array.from(new Set(res[resKey].concat(res[k])));
+      delete res[k];
+    }
+  });
+  return res;
+}
+
+
+module.exports = function autocomplete(token) {
+  return searchTokenInField('type_1', token, {})
+    .then(res => searchTokenInField('type_2', token, res))
+    .then(res => searchTokenInField('graphie_1', token, res))
+    .then(res => searchTokenInField('graphie_2', token, res))
+    .then(res => searchTokenInField('orientation', token, res))
+    .then(res => format(res));
 };
+
