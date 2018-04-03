8e3438cb46b57e24235f21739d6dc00611df21bc
Simon Meoni <simonmeoni@aol.com>
2018-04-03 17:35:53 +0200

    refactor autocomplete

--- methods/autocomplete.js
+++ methods/autocomplete.js
@@ -7,37 +7,57 @@ const client = new elasticsearch.Client({
   host: `${elasticEnv}`,
 });
 
+const fields = [
+  'type_1',
+  'graphie_1',
+  'cat_1',
+  'type_constr_1',
+  'constr_1',
+  'type_2',
+  'graphie_2',
+  'cat_2',
+  'type_constr_2',
+  'constr_2',
+  'orientation',
+  'complexite',
+];
+
+function formatBuckets(buckets) {
+  return buckets.map(el => Object.values(el.dedup_docs.hits.hits[0]._source)[0]);
+}
+
 function formatAggs(resp) {
-  const res = [];
-  resp.aggregations.dedup.buckets.forEach(el =>
-    res.push(Object.values(el.dedup_docs.hits.hits[0]._source)[0]));
+  const res = {};
+  Object.keys(resp.aggregations).forEach((agg) => {
+    res[agg] = formatBuckets(resp.aggregations[agg].buckets);
+  });
   return res;
 }
 
-function searchTokenInField(field, token, acObject) {
-  const newObject = acObject;
-  const aggs = {
-    'dedup': {
+function createAggregation() {
+  const aggs = {};
+  fields.forEach((el) => {
+    aggs[el] = {
       'terms': {
-        'size': 20,
-        'field': `${field}`,
+        'field': el,
+        'size': 30,
       },
       'aggs': {
         'dedup_docs': {
           'top_hits': {
-            'size': 1,
-            '_source': [`${field}`],
+            '_source': [el],
           },
         },
       },
-    },
-  };
+    };
+  });
+  return aggs;
+}
 
+function searchTokenInField(token) {
   const query = {
     'query_string': {
-      'fields': [
-        `${field}`,
-      ],
+      'fields': fields,
       'query': `*${token}*`,
     },
   };
@@ -48,11 +68,8 @@ function searchTokenInField(field, token, acObject) {
     body: {
       'query': query,
       'size': 0,
-      'aggs': aggs,
+      'aggs': createAggregation(),
     },
-  }).then((resp) => {
-    newObject[field] = formatAggs(resp);
-    return acObject;
   });
 }
 
@@ -72,17 +89,6 @@ function format(res) {
 
 
 module.exports = function autocomplete(token) {
-  return searchTokenInField('type_1', token, {})
-    .then(res => searchTokenInField('type_2', token, res))
-    .then(res => searchTokenInField('type_constr_1', token, res))
-    .then(res => searchTokenInField('type_constr_2', token, res))
-    .then(res => searchTokenInField('graphie_1', token, res))
-    .then(res => searchTokenInField('graphie_2', token, res))
-    .then(res => searchTokenInField('cat_1', token, res))
-    .then(res => searchTokenInField('cat_2', token, res))
-    .then(res => searchTokenInField('constr_1', token, res))
-    .then(res => searchTokenInField('constr_2', token, res))
-    .then(res => searchTokenInField('complexite', token, res))
-    .then(res => searchTokenInField('orientation', token, res))
-    .then(res => format(res));
+  return searchTokenInField(token)
+    .then(res => format(formatAggs(res)));
 };
