d83dd5d3e1054b69821e75fb94b6da124ce9a672
Simon Meoni <simonmeoni@aol.com>
2018-04-30 10:54:33 +0200

    add front networkbuilder methods to backend

--- routes/graph.js
+++ routes/graph.js
@@ -4,7 +4,7 @@ const { graph } = require('../methods/graph');
 const router = express.Router();
 
 router.get('/graph', (req, res) => {
-  graph(req.query.token)
+  graph(req.query.start, req.query.range, req.query.tokens)
     .then(resp => res.status(200).send(resp))
     .catch(resp => res.status(400).send(resp));
 });
